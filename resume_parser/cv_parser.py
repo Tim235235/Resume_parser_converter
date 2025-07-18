45b1cfbfcd776b34b67ff518d92c64dc7ca2a448
Tim235235 <124810167+Tim235235@users.noreply.github.com>
2025-07-18 10:31:50 +0200

    Update cv_parser.py

--- resume_parser/cv_parser.py
+++ resume_parser/cv_parser.py
@@ -180,13 +180,19 @@ keys = [
 nlp = spacy.load('en_core_web_lg')
 
 #----Loading pattern data----
-with open(r"Section_synonyms_text_file.txt", "r", encoding="utf-8") as file:
+import os
+
+base_dir = os.path.dirname(__file__)  # folder containing cv_parser.py
+
+# Open Section_synonyms_text_file.txt in the same folder
+with open(os.path.join(base_dir, "Section_synonyms_text_file.txt"), "r", encoding="utf-8") as file:
     content = file.read()
 section_synonyms = ast.literal_eval(content)
 
 loaded_patterns = {}
-with open(r"Regex_patterns.txt", "r", encoding="utf-8") as file:
+with open(os.path.join(base_dir, "Regex_patterns.txt"), "r", encoding="utf-8") as file:
     exec(file.read(), {}, loaded_patterns)
+
 #-----------------------------
 all_headings = [h for synonyms in section_synonyms.values() for h in synonyms]
 
